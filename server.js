47261f1ec5a82f1a3de57fa9f0ba99005ede4d21
Aaron Master <aaron@Aarons-MacBook-Pro.local>
2022-04-06 22:51:17 -0400

    chapter 5

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import helloController from "./controllers/hello-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 import userController from "./controllers/user-controller.js";
 
 const app = express();
@@ -7,6 +8,7 @@ app.use(express.json());
 
 helloController(app);
 userController(app);
+tuitsController(app);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Full Stack Development!");
