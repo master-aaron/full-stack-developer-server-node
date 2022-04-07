a16c652eb8f268be5e4666511a68de9f480452a1
Aaron Master <aaron@Aarons-MacBook-Pro.local>
2022-04-06 22:26:06 -0400

    chapter 4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 
 const app = express();
+app.use(express.json());
 
 helloController(app);
 userController(app);
